232f1cc69ac657527ab7052b0fa66b9d87de81f2
jayeeeed <jayed@myalice.ai>
2025-01-27 03:27:19 +0600

    refactor: use os.getenv instead of os.environ.get for port retrieval

--- main.py
+++ main.py
@@ -25,5 +25,5 @@ app.include_router(expense_router)
 # print(f"Public URL: {public_url}")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8001))
+    port = int(os.getenv("PORT", 8001))
     uvicorn.run(app, host="0.0.0.0", port=port)
